47c3550f6f4be8308b1a82789b0f2929175e448f
Jorge Epuñan <jorge.epunan@mercadolibre.cl>
2016-10-03 21:36:16 -0300

    Mejoras varias (#72)
    * agregado mas maneras de preguntar
    
    porque nunca me acuerdo de `gardel` y sí de `cuándo pagan`
    
    * mejor mensaje, singular/plurar
    
    robado de otro script, si falta 1 día el mensaje es en singular

--- scripts/gardel.js
+++ scripts/gardel.js
@@ -23,12 +23,13 @@ module.exports = function gardel(robot) {
   var nameLastDay = moment().locale('es').endOf('month').isBusinessDay() ? moment().locale('es').endOf('month').format('dddd') : moment().locale('es').endOf('month').prevBusinessDay().format('dddd');
   var dayCount = lastBusinessDay - today;
 
-  robot.respond(/(gardel)/, function(msg) {
+  robot.respond(/gardel|cu[aá]ndo pagan/, function(msg) {
     var message = '';
+    var plural = dayCount > 1 ? ['n','s'] : ['',''];
     if (dayCount === 0) {
       message = `:tada: Hoy pagan :tada:`;
     } else {
-      message = `Faltan ${dayCount} días para que paguen. Este mes pagan el ${lastBusinessDay}, que cae ${nameLastDay} :tired_face:`;
+      message = `Falta${plural[0]} ${dayCount} día${plural[1]} para que paguen. Este mes pagan el ${lastBusinessDay}, que cae ${nameLastDay} :tired_face:`;
     }
     return msg.send(message);
   });
