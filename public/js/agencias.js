01437162b8d118a5aeafc68413a132ba4f92c798
Jorge Epuñan <juanbrujo@gmail.com>
2016-10-08 22:52:51 -0300

    dándole colorsh a lo hecho por :leonardo:
    ayer si aporta y no me pitee nada :/ :leonardo: puedes echarle un ojo
    principalmente a agencias.js?

--- public/js/agencias.js
+++ public/js/agencias.js
@@ -1,34 +1,33 @@
-$(document).ready(function() {
-  $('#agencias-message').hide();
-  $('#agencias-preview').hide();
-  $('#agencias-404').hide();
-  $('.agencias-show').click(function() {
-    var src = $(this).data('src');
-    $(this).parent().parent().find('span').css('color', 'black');
-    $(this).parent().find('span').css('color', 'blue');
-    $('#agencias-preview').hide();
-    $('#agencias-message').text('Cargando...');
-    $('#agencias-message').show();
-    $('#agencias-preview').attr('src', src)
-    .on('load', function() {
-      $('#agencias-message').text(src);
-      $('#agencias-preview').show();
-    })
-    .on('error', function() {
-      $('#agencias-message').text('No disponible :(');
-      $('#agencias-message').show();
-    });
-  });
-  $('.agencias-delete').click(function() {
-    var src = $(this).data('src');
-    var token = $(this).data('token');
-    var result = window.confirm('¿Esta seguro que desea eliminar la imagen?');
-    if (result) {
-      window.location.href = `/agencias/delete?link=${src}&token=${token}`
-    }
-  });
-  $('.agencias-fix').click(function() {
-    var src = $(this).data('src');
-    window.location.href = `/agencias/fix?link=${src}`
+$('#agencias-404').hide();
+
+$('.agencias-show').on('click', function() {
+  var src = $(this).data('src');
+
+  $(this).parents('li').addClass('active').siblings().removeClass('active');
+
+  $('#agencias-message').text('Cargando...');
+  $('#agencias-preview').attr('src', src)
+  .on('load', function() {
+    $('#agencias-message').text(src);
+    $('.agencias-preview').show();
+  })
+  .on('error', function() {
+    $('#agencias-message').text('No disponible :(');
+    $('.agencias-preview').show();
   });
 });
+$('.agencias-delete').on('click', function() {
+  var src = $(this).data('src');
+  var token = $(this).data('token');
+  var result = window.confirm('¿Está seguro que desea eliminar la imagen?');
+
+  if (result) {
+    window.location.href = `/agencias/delete?link=${src}&token=${token}`
+  }
+});
+$('.agencias-fix').on('click', function() {
+  var src = $(this).data('src');
+
+  window.location.href = `/agencias/fix?link=${src}`
+});
+
