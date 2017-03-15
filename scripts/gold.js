1eb1216737ac5f495812d4c019e39fdb0a64ed12
Leonardo Gatica <lgatica@protonmail.com>
2017-03-15 22:03:24 +0000

    fix(typo): Se corrigio texto

--- scripts/gold.js
+++ scripts/gold.js
@@ -20,9 +20,9 @@ module.exports = robot => {
       .find(data => data.user === name)
     if (data) {
       const expire = (new Date(data.expire)).toISOString().split('T').shift()
-      res.send(`${name} es :monea: hasta el ${expire}`)
+      res.send(`${name} es golden :monea: hasta el ${expire}`)
     } else {
-      res.send(`${name} no es :monea:`)
+      res.send(`${name} no es golden :monea:`)
     }
     return
   })
@@ -32,14 +32,14 @@ module.exports = robot => {
     const keys = (process.env.GOLD_KEYS || '').split(',')
     const goldUsers = JSON.parse(robot.brain.get('gold_users') || '{}')
     if (key in goldUsers) {
-      res.send('Lo siento la key ya fue ocupada')
+      res.send('Lo siento, la key ya fue utilizada.')
     } else if (keys.includes(key)) {
       const now = new Date()
       const diff = 1000 * 60 * 60 * 24 * 30
       const expire = new Date(now.getTime() + diff)
       goldUsers[key] = {user: res.message.user.name, expire: expire}
       robot.brain.set('gold_users', JSON.stringify(goldUsers))
-      res.send('Felicidades ya eres :monea: :wink:')
+      res.send('*¡Felicitaciones!* ya eres golden :monea: :wink: :yeah:')
     } else {
       res.send('No es una clave válida')
     }
