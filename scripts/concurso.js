fbfe3b6c1dea94a8af75ff49c071cfeb0e6239a7
Jorge Epunan <jorgeepunan@Jorges-Mac-mini.local>
2017-03-16 15:52:54 -0300

    feat(txt): nueva modalidad de premio: stickers

--- scripts/concurso.js
+++ scripts/concurso.js
@@ -8,19 +8,21 @@
 //   None
 //
 // Commands:
-//   hubot concurso (txt|inicio|reglas|ganador|fin) <mensaje>
+//   hubot concurso (txt|inicio|stickers|reglas|premio|surtidos|ganador|fin) <mensaje>
+//
+// Author:
+//   @jorgeepunan
+
+// Mensajes:
 //    - txt <mensaje>: despliega un mensaje
 //    - inicio: da la intro de bienvenida
 //    - reglas: muestra las reglas
 //    - ganador <usuario sin @>: muestra al ganador con fanfarria y weas
 //    - fin: datos finales de contacto para cobrar el premio
-//
-// Author:
-//   @jorgeepunan
 
 module.exports = function(robot) {
 
-	return robot.respond(/concurso (txt|inicio|reglas|ganador|fin)?(.*)/i, function(msg) {
+	return robot.respond(/concurso (txt|inicio|stickers|reglas|premio|surtidos|ganador|fin)?(.*)/i, function(msg) {
 		var opcion 		= msg.match[1];
 		var mensaje 	= msg.match[2];
 		var userName	= msg.message.user;
@@ -29,15 +31,16 @@ module.exports = function(robot) {
 
 		// textos
 		var inicio 		= '*¡Nuevo concurso en devsChile!* ​_Adivina Quién_​ y ganarás 1 HuemulCoin gentileza de tu grupo favorito de _Slack_';
+		var stickers 		= '*¡Nuevo concurso en devsChile!* ​_Adivina Quién_​ y ganarás 1 set de 3 _stickers dev surtidos gentileza de tu grupo favorito de _Slack_';
+		var premio = "El ganador se llevará 1 HuemulCoin ℌℭ (`huemul finvox huemulcoin`) pagado mediante PayPal. (Si no tiene PayPal, se consigue uno o aprovecha de sacar una cuenta)";
+		var surtidos = "El ganador se llevará un surtido de 3 _stickers dev_, enviados por correo certificado";
 		var reglas  	= [
 		'Quienes quieran concursar tendrán que seguir las siguientes simples reglas:\n',
 '- Se darán hasta 3 pistas identificando a alguien que pertenezca a este grupo\n',
-'- Si crees saber quién es, debes etiquetar esta publicación (no en las pistas mismas) el ícono de quién crees que es la persona\n',
+'- Si crees saber quién es, debes etiquetar esta publicación (no en las pistas mismas, esta de AQUÍ) el ícono de quién crees que es la persona\n',
 '- El ganador no será el más rápido, sino por un sorteo a través de `huemul-azar` entre todos quienes etiquetaron correctamente\n',
-'- El ganador se llevará 1 HuemulCoin ℌℭ (`huemul finvox huemulcoin`) pagado mediante PayPal. (Si no tiene PayPal, se consigue uno o aprovecha de sacar una cuenta)\n',
 '- Comienza ahora, *¡ya!*'];
-		var fin 			= 'Envíe ASAP un email a huemul@devschile.cl para reclamar su premio. :huemul: :mailbox_closed: :moneybag:';
-
+		var fin 			= 'El ganador envíe ASAP un email a huemul@devschile.cl para reclamar su premio. :huemul: :mailbox_closed: :monea:';
 
 		// sanitiza output para evitar abuso de malulos. prestado de karma.coffee
 		var words = ['@here', '@channel', '@group', '@everyone'];
@@ -50,7 +53,7 @@ module.exports = function(robot) {
 
 		// verifica si usuario es uno de los 3 admin. muy rústica la validación, lo sé. prestado de karma.coffee
 		//if( userClean !== 'shell' ) { // localhost test
-		if( userClean !== 'jorgeepunan' ) {
+		if( userClean !== 'shell' ) {
 
 			msg.send('Tienes que ser admin para usar este script.');
 
@@ -58,15 +61,21 @@ module.exports = function(robot) {
 
 			if ( opcion === 'txt' ) {
 				// return robot.messageRoom('Shell', "🎉 *¡CONCURSO!* 🎉 " + mensaje); // localhost test
-				return robot.messageRoom(room, '🎉 *¡CONCURSO!* 🎉 ' + mensaje);
+				return robot.messageRoom('Shell', '🎉 *¡CONCURSO!* 🎉 ' + mensaje);
 			}  else if ( opcion === 'inicio' ) {
-				return robot.messageRoom(room, '🎉 *¡CONCURSO!* 🎉 \n' + inicio);
+				return robot.messageRoom('Shell', '🎉 *¡CONCURSO!* 🎉 \n' + inicio);
+			} else if ( opcion === 'stickers' ) {
+				return robot.messageRoom('Shell', '🎉 *¡CONCURSO!* 🎉 Reglas:\n' + stickers);
+			} else if ( opcion === 'premio' ) {
+				return robot.messageRoom('Shell', '🎉 *¡CONCURSO!* 🎉 \n' + premio);
+			} else if ( opcion === 'surtidos' ) {
+				return robot.messageRoom('Shell', '🎉 *¡CONCURSO!* 🎉 \n' + surtidos);
 			} else if ( opcion === 'reglas' ) {
-				return robot.messageRoom(room, '🎉 *¡CONCURSO!* 🎉 Reglas:\n' + reglas.join(''));
+				return robot.messageRoom('Shell', '🎉 *¡CONCURSO!* 🎉 Reglas:\n' + reglas.join(''));
 			} else if ( opcion === 'ganador' ) {
-				return robot.messageRoom(room, '🎉 *¡CONCURSO!* 🎉 :parrot: ¡TENEMOS UN GANADOR! :parrot: \n\n :trophy: :trophy: :trophy: *' + mensaje + '* :trophy: :trophy: :trophy: \n\n ¡ F E L I C I D A D E S !');
+				return robot.messageRoom('Shell', '🎉 *¡CONCURSO!* 🎉 :parrot: ¡TENEMOS UN GANADOR! :parrot: \n\n :trophy: :trophy: :trophy: *' + mensaje + '* :trophy: :trophy: :trophy: \n\n ¡ F E L I C I D A D E S !');
 			} else if ( opcion === 'fin' ) {
-				return robot.messageRoom(room, '🎉 *¡CONCURSO!* 🎉 ' + fin);
+				return robot.messageRoom('Shell', '🎉 *¡CONCURSO!* 🎉 ' + fin);
 			}
 
 		}
