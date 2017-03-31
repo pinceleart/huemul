4e36f33526d0a16b67e5a48a3b01b3d5fe7bd73a
Leonardo Gatica <lgatica@protonmail.com>
2017-03-31 21:47:48 +0000

    feat(gold): Se agrego comando y webhook para agregar usuarios golden

--- scripts/gold.js
+++ scripts/gold.js
@@ -5,25 +5,34 @@
 //   None
 //
 // Configuration:
-//   None
+//   GOLD_USER_AGENT, GOLD_SECRET, GOLD_KEYS
 //
 // Commands:
 //   hubot gold status <name>
 //   hubot gold insert <key>
+//   hubot gold add <user>
 
 module.exports = robot => {
   robot.respond(/gold status (.*)/i, res => {
     const name = res.match[1]
     const goldUsers = JSON.parse(robot.brain.get('gold_users') || '{}')
-    const data = Object.keys(goldUsers)
-      .map(key => goldUsers[key])
-      .find(data => data.user === name)
-    if (data) {
-      const expire = (new Date(data.expire)).toISOString().split('T').shift()
-      res.send(`${name} es golden :monea: hasta el ${expire}`)
-    } else {
-      res.send(`${name} no es golden :monea:`)
+    const result = Object.keys(goldUsers)
+      .map(key => ({key: key, data: goldUsers[key]}))
+      .find(result => result.data.user === name)
+    if (result) {
+      const now = new Date()
+      const expireDate = new Date(result.data.expire)
+      if (now <= expireDate) {
+        const expire = expireDate.toISOString().split('T').shift()
+        res.send(`${name} es golden :monea: hasta el ${expire}`)
+        return
+      } else {
+        delete goldUsers[result.key]
+        robot.brain.set('gold_users', JSON.stringify(goldUsers))
+        res.send(`${name} ya no eres golden :monea:, expiró el ${expire}`)
+      }
     }
+    res.send(`${name} no es golden :monea:`)
     return
   })
 
@@ -35,13 +44,47 @@ module.exports = robot => {
       res.send('Lo siento, la key ya fue utilizada.')
     } else if (keys.includes(key)) {
       const now = new Date()
-      const diff = 1000 * 60 * 60 * 24 * 30
+      const diff = 1000 * 60 * 60 * 24 * 30 // 30 días
       const expire = new Date(now.getTime() + diff)
       goldUsers[key] = {user: res.message.user.name, expire: expire}
       robot.brain.set('gold_users', JSON.stringify(goldUsers))
       res.send('*¡Felicitaciones!* ya eres golden :monea: :wink: :yeah:')
     } else {
       res.send('No es una clave válida')
+    }
+    return
+  })
+
+  const addUser = user => {
+    const goldUsers = JSON.parse(robot.brain.get('gold_users') || '{}')
+    const now = new Date()
+    const diff = 1000 * 60 * 60 * 24 * 60 // 60 días
+    const expire = new Date(now.getTime() + diff)
+    goldUsers[user] = {user: user, expire: expire}
+    robot.brain.set('gold_users', JSON.stringify(goldUsers))
+    const comunidad = robot.adapter.client.rtm.dataStore.getChannelByName('#comunidad')
+    const message = `:clap2: *${name}* donó 1 mes de servidor a :huemul:, se lleva 3 stickers :huemul: y es miembro golden :monea: por 2 meses!`
+    robot.send({room: comunidad.id}, message)
+  }
+
+  robot.respond(/gold add (.*)/i, res => {
+    const isAdmin = robot.auth.isAdmin(res.message.user)
+    const hasRole = robot.auth.hasRole(res.message.user, 'gold')
+    if (isAdmin || hasRole) {
+      addUser(res.match[1])
+    }
+    return
+  })
+
+  robot.router.post('/gold/webhook', (req, res) => {
+    if (req.get('User-Agent') === process.env.GOLD_USER_AGENT && req.body.secret === process.env.GOLD_SECRET) {
+      const user = robot.adapter.client.rtm.dataStore.getUserByEmail(req.body.email)
+      if (typeof user !== 'undefined') {
+        addUser(user.name)
+      }
+      res.send('Ok')
+    } else {
+      res.send('Error')
     }
     return
   })
