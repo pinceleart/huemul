df6dda1e978f136ce716021963dce4af1e78f52f
Hola <hola@andalaosa.cl>
2017-04-16 21:27:11 -0300

    chore(js): pasando de coffee a js y limpiando scripts que no se usan

--- scripts/errorcodes.js
+++ scripts/errorcodes.js
@@ -15,9 +15,10 @@ const codes = [
 ]
 
 module.exports = robot => {
+
   robot.hear(/error (\d{3})(\b|ing|e?d)/, res => {
     const code = parseInt(res.match[1], 10);
-    console.log(code)
     if (codes.includes(code)) res.send(`https://http.cat/${code}`)
-  })
+  });
+
 }
---
+++ scripts/flipper.js
@@ -0,0 +1,24 @@
+// Description:
+//   Imprime una url a un gif de table flipping
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot flip
+//
+// Author:
+//   @hectorpalmatellez
+
+module.exports = function(robot) {
+  robot.respond(/flip/i, function(msg) {
+    var url = 'http://tableflipper.com/json';
+    msg.robot.http(url).get()(function(err, res, body) {
+      var data = JSON.parse(body);
+      msg.send(data.gif);
+    });
+  });
+};
