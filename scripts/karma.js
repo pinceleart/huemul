9b19e47008813644a9a7058219f013f9f5ee8212
Leonardo Gatica <lgatica@protonmail.com>
2017-05-12 09:27:03 -0300

    fix(karma): Se corrigio puntuación

--- scripts/karma.js
+++ scripts/karma.js
@@ -184,7 +184,7 @@ module.exports = robot => {
     } else if (targetToken.toLowerCase().split(' ')[0] === 'reset') {
       const thisUser = response.message.user
       if (thisUser.name.toLowerCase() !== 'hector') {
-        return response.send('Tienes que ser :hector: para realizar esta función')
+        return response.send('Tienes que ser :hector: para realizar esta función.')
       }
       const resetCommand = targetToken.toLowerCase().split(' ')[1]
       if (!resetCommand) return
