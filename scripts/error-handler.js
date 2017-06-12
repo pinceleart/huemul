66cf7045c545c6fcdc7899d95bb9206c1d2a5fdf
Leonardo Gatica <lgatica@protonmail.com>
2017-06-12 15:18:15 +0000

    feat(sentry): se agrego enviar errores a sentry
    Es necesario configurar la variable de entorno SENTRY_DSN

---
+++ scripts/error-handler.js
@@ -0,0 +1,41 @@
+// Description:
+//   Send errors to sentry.io
+//
+// Dependencies:
+//   raven
+//
+// Configuration:
+//   SENTRY_DSN, SENTRY_ENVIRONMENT, SENTRY_NAME
+//
+// Commands:
+//   None
+
+const Raven = require('raven')
+
+module.exports = robot => {
+  if (!process.env.SENTRY_DSN) {
+    robot.logger.warning(
+      'The SENTRY_DSN environment variable not set. Sentry not configured.')
+  }
+
+  Raven.config().install()
+
+  robot.error((err, res) => {
+    robot.logger.error(err)
+    if (typeof res !== 'undefined' && res !== null) {
+      if (['SlackBot', 'Room'].includes(robot.adapter.constructor.name) && res.message) {
+        Raven.setContext({
+          user: {
+            id: res.message.user.id,
+            username: res.message.user.name,
+            email: res.message.user.email
+          }
+        })
+      }
+      if (typeof res.reply === 'function') {
+        res.reply(`an error has occurred: \`${err.message}\``)
+      }
+    }
+    Raven.captureException(err)
+  })
+}
