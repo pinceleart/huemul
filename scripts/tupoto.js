93f3f514e715bbe3713461e25e7810dfe6f7fae2
Víctor San Martín <vsanmartinm@gmail.com>
2017-07-29 01:13:33 -0400

    tupo.to shortener url

---
+++ scripts/tupoto.js
@@ -0,0 +1,44 @@
+// Description:
+//   Devuelve una url corta generada por tupo.to
+//
+// Dependencies:
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot tupo.to URL
+//   hubot tupoto URL
+//   hubot tupotoizame URL
+//   hubot tupotizame URL
+//
+// Author:
+//   @victorsanmartin
+
+module.exports = robot => {
+  function checkUrl(url) {
+    return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
+  }
+
+  robot.respond(/tupo(\.to|to|toizame|tizame) (.*)/i, msg => {
+    const url = msg.match[2];
+
+    if (!checkUrl(url)) {
+      msg.send(`tupo.to solo funciona con url, y ${url} parece no serlo`);
+      return;
+    }
+
+    const data = JSON.stringify({uri: url});
+
+    robot.http('https://tupo.to/').post(data)
+    ((err, res, body) => {
+      if (err) {
+        robot.emit('error', err, msg);
+      } else {
+        msg.send(
+          `Toma tupo.to: ${body}`
+        );
+      }
+    });
+  });
+};
