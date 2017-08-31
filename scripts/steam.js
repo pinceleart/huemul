0fc3d1d182f8e8c3541009e113a85bd8f3ec044f
Hola <hola@andalaosa.cl>
2017-08-31 12:04:06 -0300

    chore(js): several code improvements
    - const all the things
    - better command handle, if moved to a broader scope
    - some typo fixes
    
    test:
    
    `huemul steam` -> error msg
    `huemul steam lala` -> error msg
    `huemul steam daily` -> returns offer

--- scripts/steam.js
+++ scripts/steam.js
@@ -4,7 +4,6 @@
 
 // Dependencies:
 //   "cheerio": "latest"
-//   "sanitize-html": "latest"
 //
 
 // Commands:
@@ -19,39 +18,42 @@ const cheerio = require('cheerio');
 
 module.exports = (robot) => {
 
-  robot.respond(/steam (.*)/i, (msg) => {
-    var args = msg.match[1];
+  robot.respond(/steam(.*)/i, (msg) => {
+    const args = msg.match[1].split(' ')[1];
 
-    robot.http('http://store.steampowered.com').get()(function(err, res, body) {
-      if (err || res.statusCode !== 200) {
-        msg.send('Actualmente Steam no responde!');
-        return robot.emit('error', err || new Error(`Status code ${res.statusCode}`), msg)
-      }
+    if (args === 'daily') {
 
-      let $ = cheerio.load(body);
-      let idAttr = $('.dailydeal_desc .dailydeal_countdown').attr('id');
-      let id = idAttr.substr(idAttr.length - 6);
-      let url = `http://store.steampowered.com/api/appdetails/?appids=${id}`;
-
-      robot.http(url).get()(function(err, res, body) {
+      robot.http('http://store.steampowered.com').get()(function(err, res, body) {
         if (err || res.statusCode !== 200) {
-          msg.send('Actualmente Steam no responde!');
+          msg.send('Actualmente _Steam_ no responde.');
           return robot.emit('error', err || new Error(`Status code ${res.statusCode}`), msg)
         }
 
-        let game = JSON.parse(body)[id].data;
-        let name = game.name;
-        let price = game.price_overview;
-        let final = price.final / 100;
-        let initial = price.initial / 100;
-        let discount = price.discount_percent;
+        const $ = cheerio.load(body);
+        const idAttr = $('.dailydeal_desc .dailydeal_countdown').attr('id');
+        const id = idAttr.substr(idAttr.length - 6);
+        const url = `http://store.steampowered.com/api/appdetails/?appids=${id}`;
 
-         if (args === 'daily') {
-            msg.send(`https://store.steampowered.com/app/${id}`);
-            msg.send(`Oferta del dia: ${name}, a solo $CLP ${final}. Valor original $CLP ${initial}, eso es un -${discount}%!`);
-        }
+        robot.http(url).get()(function(err, res, body) {
+          if (err || res.statusCode !== 200) {
+            msg.send('Actualmente Steam no responde!');
+            return robot.emit('error', err || new Error(`Status code ${res.statusCode}`), msg)
+          }
 
+          const game = JSON.parse(body)[id].data;
+          const name = game.name;
+          const price = game.price_overview;
+          const final = price.final / 100;
+          const initial = price.initial / 100;
+          const discount = price.discount_percent;
+
+          msg.send(`Oferta del día: ${name}, a sólo $CLP ${final}. Valor original $CLP ${initial}, eso es un -${discount}%! https://store.steampowered.com/app/${id}`);
+
+        });
       });
-    });
+
+    } else {
+      msg.send(`Para obtener la oferta del día en _Steam_ debes usar el comando: *huemul steam daily*`)
+    }
   });
 };
