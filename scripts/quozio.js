24f089e3bbb93c1f813f089d151f408659863c6e
Leonardo Gatica <lgatica@protonmail.com>
2017-09-14 12:24:08 -0300

    feat(scripts): se agrego script quozio
    El script permite generar quote image desde quozio
    El comando es `hubot quozio <user> dijo <quote>`

---
+++ scripts/quozio.js
@@ -0,0 +1,53 @@
+// Description:
+//   Generate quote image from quozio
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot quozio <user> dijo <quote>
+//
+// Author:
+//   @lgaticaq
+
+module.exports = robot => {
+  const getRandomTheme = () => {
+    const min = Math.ceil(1000);
+    const max = Math.floor(2000);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  robot.respond(/quozio (.+) dijo (.+)/i, res => {
+    const query = {
+      q: res.match[1].trim(),
+      a: res.match[2].trim()
+    }
+    if (query.q === '' || query.a === '') return
+    console.log(query)
+    robot.http('http://quozio.com/fetch/getQuoteRef.aspx').query(query).get()((err, response, body) => {
+      let errMessage
+      console.log(err, response.statusCode, body)
+      if (err) {
+        robot.emit('error', err, res)
+        errMessage = err.message
+      }
+      if (response.statusCode !== 200) {
+        errMessage = `Wrong status code ${response.statusCode}`
+      }
+      if (errMessage) {
+        return res.reply(`ocurrio el siguiente error: ${errMessage}`)
+      }
+      try {
+        const data = JSON.parse(body)
+        const theme = getRandomTheme()
+        res.send(`https://d3kvsdrdan3wbb.cloudfront.net/img/${data.ref}/${theme}/${query.q}.jpg`)
+      } catch (err) {
+        robot.emit('error', err, res)
+        return res.reply(`ocurrio el siguiente error: ${err.message}`)
+      }
+    })
+  })
+}
