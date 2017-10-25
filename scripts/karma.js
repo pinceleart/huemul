f14fea24507eed77ec8f02fa2d427878228d3123
Hola <hola@andalaosa.cl>
2017-10-25 10:06:47 -0300

    chore(html): better karma design for logs
    test:
    - `https://huemul.devschile.cl/huemul/karma/log`
    - `https://huemul.devschile.cl/huemul/karma/log/claudrocker`

--- scripts/karma.js
+++ scripts/karma.js
@@ -266,7 +266,34 @@ module.exports = robot => {
       return line
     })
     res.setHeader('content-type', 'text/html')
-    res.end(`Karmalog:\n<ul><li>${processedKarmaLog.join('</li><li>')}</li></ul>`)
+    res.end(`<html>
+<head>
+  <title>devsChile - Karma Log</title>
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Inconsolata:400,700"/>
+  <link rel="stylesheet" type="text/css" href="//cdn.rawgit.com/mutable-tools/MutaGrid/master/demo/mutagrid/dist/5/mutagrid.min.css"/>
+  <style>body,html{height:100%;box-sizing:border-box}html{overflow-x:hidden}body{background:#000;color:#ddd;font-size:16px}body,code,pre{font-family:Inconsolata,monospace}code,h1,h2,h3,pre{color:#fff;font-weight:400}a{color:#e74c3c}.text-center{text-align:center}main{padding:5em 1.5em}h1{font-size:18px}h3{margin-top:20px}h2,h3{font-size:16px}hr{opacity:.4}</style>
+</head>
+<body>
+  <main class="container">
+    <div class="row">
+      <div class="column-5 column-center text-center">
+        <h1>devsChile - Karma Log</h1>
+        <hr/>
+        <h2>Listado del karma de los usuarios de devschile.cl</h2>
+        <hr/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column-2 column-offset-2">
+        <ul>
+          <li>${processedKarmaLog.join('</li><li>')}</li>
+        </ul>
+      </div>
+    </div>
+  </main>
+</body>
+</html>`
+    )
   })
 
   robot.router.get(`/${robot.name}/karma/log/:user`, (req, res) => {
@@ -284,6 +311,31 @@ module.exports = robot => {
       msg = `Karmalog:\n<ul><li>${processedKarmaLog.join('</li><li>')}</li></ul>`
     } else { msg = `No hay detalles sobre el karma de ${req.params.user}` }
     res.setHeader('content-type', 'text/html')
-    res.end(msg)
+    res.end(`<html>
+<head>
+  <title>devsChile - Karma Log ${req.params.user}</title>
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Inconsolata:400,700"/>
+  <link rel="stylesheet" type="text/css" href="//cdn.rawgit.com/mutable-tools/MutaGrid/master/demo/mutagrid/dist/5/mutagrid.min.css"/>
+  <style>body,html{height:100%;box-sizing:border-box}html{overflow-x:hidden}body{background:#000;color:#ddd;font-size:16px}body,code,pre{font-family:Inconsolata,monospace}code,h1,h2,h3,pre{color:#fff;font-weight:400}a{color:#e74c3c}.text-center{text-align:center}main{padding:5em 1.5em}h1{font-size:18px}h3{margin-top:20px}h2,h3{font-size:16px}hr{opacity:.4}</style>
+</head>
+<body>
+  <main class="container">
+    <div class="row">
+      <div class="column-5 column-center text-center">
+        <h1>devsChile - Karma Log ${req.params.user}</h1>
+        <hr/>
+        <h2>Listado del karma de los usuarios de devschile.cl</h2>
+        <hr/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column-2 column-offset-2">
+        ${msg}
+      </div>
+    </div>
+  </main>
+</body>
+</html>`
+    )
   })
 }
