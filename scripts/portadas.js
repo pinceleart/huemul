6d9e785222a9fbfacab569a4a28b8eb4417c5928
Rafael Poveda <raerpo@gmail.com>
2017-11-18 12:06:07 -0300

    extraer diarios para evitar que sean redefinidos en cada llamada

--- scripts/portadas.js
+++ scripts/portadas.js
@@ -55,6 +55,181 @@ const listaPortadas = () => {
   `
 }
 
+const diarios = {
+  lun: {
+    url: 'http://img.kiosko.net/#DATE#/cl/cl_ultimas_noticias.750.jpg',
+    noSlashes: false
+  },
+  mercurio: {
+    url: 'http://img.kiosko.net/#DATE#/cl/cl_mercurio.750.jpg',
+    noSlashes: false
+  },
+  cuarta: {
+    url: 'http://img.kiosko.net/#DATE#/cl/cl_cuarta.750.jpg',
+    noSlashes: false
+  },
+  estrellaarica: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaArica/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  estrellaiquique: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/EstellaIquique/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  mercuriocalama: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/MercurioCalama/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  estrellaloa: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaLoa/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  estrellatocopilla: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaTocopilla/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  mercurioantofa: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/ElMercuriodeAntofagasta/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  estrellaantofa: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaAntofagasta/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  diarioatacama: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/DiarioAtacama/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  mercuriovalpo: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/MercurioValparaiso/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  estrellavalpo: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaValparaiso/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  estrellaquillota: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaQuillota/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  lider: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/LiderSanAntonio/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  lidersanantonio: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/LiderSanAntonio/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  hoyxhoy: {
+    url: endpointHxh,
+    noSlashes: false
+  },
+  hxh: {
+    url: endpointHxh,
+    noSlashes: false
+  },
+  sur: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/ElSur/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  estrellaconce: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaConcepcion/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  cronicachillan: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/CronicaChillan/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  australtemuco: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/AustralTemuco/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  australlosrios: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/AustralValdivia/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  australvaldivia: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/AustralValdivia/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  australosorno: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/AustralOsorno/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  llanquihue: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/Llanquihue/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  estrellachiloe: {
+    url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaChiloe/01-550.jpg?fecha=#DATE#',
+    noSlashes: true
+  },
+  globo: {
+    url: 'http://img.kiosko.net/#DATE#/br/br_oglobo.750.jpg',
+    noSlashes: false
+  },
+  folha: {
+    url: 'http://img.kiosko.net/#DATE#/br/br_folha_spaulo.750.jpg',
+    noSlashes: false
+  },
+  tiempo: {
+    url: 'http://img.kiosko.net/#DATE#/co/co_eltiempo.750.jpg',
+    noSlashes: false
+  },
+  paisuruguay: {
+    url: 'http://www.elpais.com.uy/printed-home/#DATE#/portada_impresa.jpg',
+    noSlashes: true
+  },
+  paisuru: {
+    url: 'http://www.elpais.com.uy/printed-home/#DATE#/portada_impresa.jpg',
+    noSlashes: true
+  },
+  paisuy: {
+    url: 'http://www.elpais.com.uy/printed-home/#DATE#/portada_impresa.jpg',
+    noSlashes: true
+  },
+  financiero: {
+    url: 'http://img.kiosko.net/#DATE#/mx/mx_financiero.750.jpg',
+    noSlashes: false
+  },
+  wallstreetjournal: {
+    url: 'http://img.kiosko.net/#DATE#/eur/wsj.750.jpg',
+    noSlashes: false
+  },
+  wsj: {
+    url: 'http://img.kiosko.net/#DATE#/eur/wsj.750.jpg',
+    noSlashes: false
+  },
+  washingtonpost: {
+    url: 'http://img.kiosko.net/#DATE#/us/washington_post.750.jpg',
+    noSlashes: false
+  },
+  usatoday: {
+    url: 'http://img.kiosko.net/#DATE#/us/usa_today.750.jpg',
+    noSlashes: false
+  },
+  monde: {
+    url: 'http://www.lemonde.fr/journalelectronique/donnees/libre/#DATE#/QUO/img_pleinepage/1.jpg',
+    noSlashes: true
+  },
+  pais: {
+    url: 'http://img.kiosko.net/#DATE#/es/elpais.750.jpg',
+    noSlashes: false
+  },
+  corrieredellasera: {
+    url: 'http://img.kiosko.net/#DATE#/it/corriere_della_sera.750.jpg',
+    noSlashes: false
+  },
+  corriere: {
+    url: 'http://img.kiosko.net/#DATE#/it/corriere_della_sera.750.jpg',
+    noSlashes: false
+  },
+  times: {
+    url: 'http://img.kiosko.net/#DATE#/uk/the_times.750.jpg',
+    noSlashes: false
+  }
+}
+
 const formatDate = (date, noSlashes = false) => {
   if (noSlashes) {
     return date.format('YYYYMMDD')
@@ -116,180 +291,6 @@ module.exports = robot => {
       .replace(/losríos$/, 'losrios')
       .replace(/chiloé$/, 'chiloe')
 
-    const diarios = {
-      lun: {
-        url: 'http://img.kiosko.net/#DATE#/cl/cl_ultimas_noticias.750.jpg',
-        noSlashes: false
-      },
-      mercurio: {
-        url: 'http://img.kiosko.net/#DATE#/cl/cl_mercurio.750.jpg',
-        noSlashes: false
-      },
-      cuarta: {
-        url: 'http://img.kiosko.net/#DATE#/cl/cl_cuarta.750.jpg',
-        noSlashes: false
-      },
-      estrellaarica: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaArica/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      estrellaiquique: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/EstellaIquique/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      mercuriocalama: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/MercurioCalama/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      estrellaloa: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaLoa/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      estrellatocopilla: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaTocopilla/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      mercurioantofa: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/ElMercuriodeAntofagasta/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      estrellaantofa: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaAntofagasta/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      diarioatacama: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/DiarioAtacama/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      mercuriovalpo: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/MercurioValparaiso/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      estrellavalpo: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaValparaiso/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      estrellaquillota: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaQuillota/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      lider: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/LiderSanAntonio/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      lidersanantonio: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/LiderSanAntonio/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      hoyxhoy: {
-        url: endpointHxh,
-        noSlashes: false
-      },
-      hxh: {
-        url: endpointHxh,
-        noSlashes: false
-      },
-      sur: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/ElSur/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      estrellaconce: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaConcepcion/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      cronicachillan: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/CronicaChillan/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      australtemuco: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/AustralTemuco/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      australlosrios: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/AustralValdivia/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      australvaldivia: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/AustralValdivia/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      australosorno: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/AustralOsorno/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      llanquihue: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/Llanquihue/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      estrellachiloe: {
-        url: 'http://edicionimpresa.soychile.cl/portadas/EstrellaChiloe/01-550.jpg?fecha=#DATE#',
-        noSlashes: true
-      },
-      globo: {
-        url: 'http://img.kiosko.net/#DATE#/br/br_oglobo.750.jpg',
-        noSlashes: false
-      },
-      folha: {
-        url: 'http://img.kiosko.net/#DATE#/br/br_folha_spaulo.750.jpg',
-        noSlashes: false
-      },
-      tiempo: {
-        url: 'http://img.kiosko.net/#DATE#/co/co_eltiempo.750.jpg',
-        noSlashes: false
-      },
-      paisuruguay: {
-        url: 'http://www.elpais.com.uy/printed-home/#DATE#/portada_impresa.jpg',
-        noSlashes: true
-      },
-      paisuru: {
-        url: 'http://www.elpais.com.uy/printed-home/#DATE#/portada_impresa.jpg',
-        noSlashes: true
-      },
-      paisuy: {
-        url: 'http://www.elpais.com.uy/printed-home/#DATE#/portada_impresa.jpg',
-        noSlashes: true
-      },
-      financiero: {
-        url: 'http://img.kiosko.net/#DATE#/mx/mx_financiero.750.jpg',
-        noSlashes: false
-      },
-      wallstreetjournal: {
-        url: 'http://img.kiosko.net/#DATE#/eur/wsj.750.jpg',
-        noSlashes: false
-      },
-      wsj: {
-        url: 'http://img.kiosko.net/#DATE#/eur/wsj.750.jpg',
-        noSlashes: false
-      },
-      washingtonpost: {
-        url: 'http://img.kiosko.net/#DATE#/us/washington_post.750.jpg',
-        noSlashes: false
-      },
-      usatoday: {
-        url: 'http://img.kiosko.net/#DATE#/us/usa_today.750.jpg',
-        noSlashes: false
-      },
-      monde: {
-        url: 'http://www.lemonde.fr/journalelectronique/donnees/libre/#DATE#/QUO/img_pleinepage/1.jpg',
-        noSlashes: true
-      },
-      pais: {
-        url: 'http://img.kiosko.net/#DATE#/es/elpais.750.jpg',
-        noSlashes: false
-      },
-      corrieredellasera: {
-        url: 'http://img.kiosko.net/#DATE#/it/corriere_della_sera.750.jpg',
-        noSlashes: false
-      },
-      corriere: {
-        url: 'http://img.kiosko.net/#DATE#/it/corriere_della_sera.750.jpg',
-        noSlashes: false
-      },
-      times: {
-        url: 'http://img.kiosko.net/#DATE#/uk/the_times.750.jpg',
-        noSlashes: false
-      }
-    }
     if (['lista', 'help'].includes(nombre)) {
       res.send(listaPortadas())
     } else if (nombre in diarios) {
