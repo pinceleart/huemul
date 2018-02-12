3d52dfbbf9a0f20a95beb2d345048fb4030c4f49
Rafael Poveda <raerpo@gmail.com>
2018-02-12 17:07:16 -0300

    mas ciudades para buscar vuelos

--- scripts/vuelos-baratos.js
+++ scripts/vuelos-baratos.js
@@ -35,16 +35,37 @@ const cityCodes = {
   // Colombia
   bogota: 'BOG',
   medellin: 'EOH',
+  'cartagena de indias': 'ctg',
   'san andres': 'ADZ',
   // Argentina
   'buenos aires': 'BUE',
   mendoza: 'MDZ',
-  // USA
+  // Peru
+  lima: 'lim',
+  // Brasil
+  'rio de janeiro': 'rio',
+  'sao paulo': 'sao',
+  // Uruguay
+  montevideo: 'mvd',
+
+  // Norte america
   'new york': 'NYC',
+  miami: 'mia',
+  'san francisco': 'sfo',
+  'las vegas': 'las',
+  mexico: 'mex',
+  montreal: 'ymq',
+
   // Europa
   madrid: 'mad',
   roma: 'rom',
-  paris: 'par'
+  paris: 'par',
+  berlin: 'ber',
+  moscu: 'mow',
+  barcelona: 'bcn',
+  // Asia
+  tokyo: 'tyo',
+  beijing: 'bjs'
 }
 
 module.exports = robot => {
