f20bb43181e86831ff59489ccc6fca795052b4a9
Rafael Poveda <raerpo@gmail.com>
2018-02-15 17:33:16 -0300

    agregar ciudad a la respuesta para evitar confusiones

--- scripts/vuelos-baratos.js
+++ scripts/vuelos-baratos.js
@@ -35,7 +35,7 @@ module.exports = robot => {
         msg.send(json.error)
         return
       }
-      msg.send(`Encontré vuelos desde CLP ${json.price}`)
+      msg.send(`Encontré vuelos a ${msg.match[1]} desde CLP ${json.price}`)
       msg.send(`Se puede comprar aquí: ${json.url}`)
     })
   })
