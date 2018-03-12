4e54763e3072a38422c7a0b0bbb38139237ce8bb
Héctor Palma Téllez <hector@palmatellez.cl>
2018-03-12 09:34:51 -0300

    Add image to nosvimo script

--- scripts/nosvimoh.js
+++ scripts/nosvimoh.js
@@ -13,16 +13,17 @@
 // Author:
 //   @pinceleart
 
-let images = [
-  "https://memecrunch.com/meme/B521D/chao-nos-vimo/image.gif",
-  "https://media.tenor.com/images/03fb6a1872602d54b6d9c42efce45a05/tenor.gif",
-  "http://66.media.tumblr.com/884bd93f973ef44ba90f3f47f8ac87d2/tumblr_njguz3If0q1rw80ozo1_400.gif",
-  "http://generadordememesonline.com/media/created/2w7wb5.png",
-  "https://i.giphy.com/VsXhOdCYnpw1q.gif",
-  "http://generadordememesonline.com/media/created/xtz4kp.jpg",
-  "http://pa1.narvii.com/6479/0518a785aafb86cfb2a10be7151d58c944d10019_hq.gif"
-];
+const images = [
+  'https://memecrunch.com/meme/B521D/chao-nos-vimo/image.gif',
+  'https://media.tenor.com/images/03fb6a1872602d54b6d9c42efce45a05/tenor.gif',
+  'http://66.media.tumblr.com/884bd93f973ef44ba90f3f47f8ac87d2/tumblr_njguz3If0q1rw80ozo1_400.gif',
+  'http://generadordememesonline.com/media/created/2w7wb5.png',
+  'https://i.giphy.com/VsXhOdCYnpw1q.gif',
+  'http://generadordememesonline.com/media/created/xtz4kp.jpg',
+  'http://pa1.narvii.com/6479/0518a785aafb86cfb2a10be7151d58c944d10019_hq.gif',
+  'https://i.imgur.com/l9tiizT.jpg'
+]
 
 module.exports = robot => {
-  robot.respond(/nosvimo/gi, msg => msg.send(msg.random(images)));
-};
+  robot.respond(/nosvimo/gi, msg => msg.send(msg.random(images)))
+}
