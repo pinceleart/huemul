16ddd7c6a678906aff5e4efc695a22708e02da00
Leonardo Gatica <lgatica@protonmail.com>
2018-04-23 10:32:49 -0300

    fix(gardel): se corrige cálculo de días

--- scripts/gardel.js
+++ scripts/gardel.js
@@ -13,26 +13,35 @@
 // Author:
 //   @hectorpalmatellez
 
-var moment = require('moment-business-days');
+var moment = require('moment-business-days')
 
 module.exports = function gardel(robot) {
-  'use strict';
+  'use strict'
 
-  moment.locale('es');
-
+  moment.locale('es')
 
   robot.respond(/gardel|cu[aá]ndo pagan/, function(msg) {
-    var today = moment();
-    var lastBusinessDay = moment().endOf('month').isBusinessDay() ? moment().endOf('month') : moment().endOf('month').prevBusinessDay();
-    var dayMessage = moment.duration(lastBusinessDay.diff(today)).humanize();
-    var dayCount = lastBusinessDay.diff(today, 'days');
-    var message = '';
-    var plural = dayCount > 1 ? 'n' : '';
+    var today = moment(`${moment().format('YYYY-MM-DD')}T00:00:00-03:00`)
+    var lastBusinessDayMoment = moment()
+      .endOf('month')
+      .isBusinessDay()
+      ? moment().endOf('month')
+      : moment()
+          .endOf('month')
+          .prevBusinessDay()
+    var dateLastBusinessDay = lastBusinessDayMoment.format('YYYY-MM-DD')
+    var lastBusinessDay = moment(`${dateLastBusinessDay}T00:00:00-03:00`)
+    var dayMessage = moment.duration(lastBusinessDay.diff(today)).humanize()
+    var dayCount = lastBusinessDay.diff(today, 'days')
+    var message = ''
+    var plural = dayCount > 1 ? 'n' : ''
     if (dayCount === 0) {
-      message = `:tada: Hoy pagan :tada:`;
+      message = `:tada: Hoy pagan :tada:`
     } else {
-      message = `Falta${plural} ${dayMessage} para que paguen. Este mes pagan el ${lastBusinessDay.format('D')}, que cae ${lastBusinessDay.format('dddd')} :tired_face:`;
+      message = `Falta${plural} ${dayMessage} para que paguen. Este mes pagan el ${lastBusinessDay.format(
+        'D'
+      )}, que cae ${lastBusinessDay.format('dddd')} :tired_face:`
     }
-    return msg.send(message);
-  });
-};
+    return msg.send(message)
+  })
+}
